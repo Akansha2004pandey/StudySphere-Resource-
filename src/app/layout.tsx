daaeee0a923064bb9778b83e98ba465c6a220c87
Akansha <akanshapandey882004@gmail.com>
2025-02-19 10:24:32 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AuthProvider from "@/context/Authprovider";
 import { Toaster } from "@/components/ui/toaster";
-
+import { Analytics } from '@vercel/analytics/next';
 import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +33,7 @@ export default function RootLayout({
       > 
         {children}
         <Toaster/>
+        <Analytics />
       </body>
     </html>
     </AuthProvider>
